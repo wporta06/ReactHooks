c3c203efb63822d756cd0761d95cf51be5986097
wporta06 <portawalid@gmail.com>
2022-02-06 13:52:07 +0100

    first commit

---
+++ help.txl
@@ -0,0 +1,6 @@
+npx creat-react-app starwars
+
+
+npm i --save react-query
+
+
---
+++ src/components/Planets.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import Planet from './Planet';
+
+const fetchPlanets = async () => {
+  const res = await fetch('http://swapi.dev/api/planets/');
+  return res.json();
+}
+
+const Planets = () => {
+  // planets in useQuery is just a key
+  const { data, status } = useQuery('planets', fetchPlanets);
+  console.log(data);
+
+  return (
+    <div>
+      <h2>Planets</h2>
+
+      {status === 'loading' && (
+        <div>Loading data</div>
+      )}
+
+      {status === 'error' && (
+        <div>Error fetching data</div>
+      )}
+
+      {status === 'success' && (
+        <div>
+        {/* results is inside data from API */}
+          { data.results.map(planet => <Planet key={planet.name} planet={planet} /> ) }
+        </div>
+      )} 
+    </div>
+  );
+}
+ 
+export default Planets;
